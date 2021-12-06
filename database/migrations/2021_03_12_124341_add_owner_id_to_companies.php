12ae9a53581074edcad5273f7d6cede0bef36f21
Joschua H <joschua.hass.sh@gmail.com>
2021-12-06 15:16:03 +0100

    stuff

--- database/migrations/2021_03_12_124341_add_owner_id_to_companies.php
+++ database/migrations/2021_03_12_124341_add_owner_id_to_companies.php
@@ -14,7 +14,7 @@ class AddOwnerIdToCompanies extends Migration
     public function up()
     {
         Schema::table('companies', function (Blueprint $table) {
-            $table->unsignedInteger("owner_id");
+            $table->unsignedInteger("owner_id")->default(0);
         });
     }
 
