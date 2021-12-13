250ad49f1b90990f3e5500eb1c10192b20112ffe
Joschua H <joschua.hass.sh@gmail.com>
2021-12-13 11:02:04 +0100

    added LicenseKey middleware

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/status', [\App\Http\Controllers\API\ServiceStatusController::class, 'index']);
 
-Route::middleware(['auth:sanctum'])->group(function (){
+Route::middleware(['auth:sanctum', 'licenseKey'])->group(function (){
 
     // User System
     Route::get('/user', function (Request $request) {
