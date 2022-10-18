3ea322b6408ed27ac92f0d7a8b71b9b640804f0b
Joschua H <54814804+joschiservice@users.noreply.github.com>
2022-10-18 16:12:33 +0200

    feat: added MinimizeJobDataEntries job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\ReduceJobDataEntries;
+use App\Jobs\MinimizeJobDataEntries;
 use App\Jobs\RemoveDeletedUsers;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -27,6 +28,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new RemoveDeletedUsers)->daily();
+        $schedule->job(new MinimizeJobDataEntries)->daily();
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
