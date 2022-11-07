4b512e4014d347a294a8119ee7051d22cc70a62b
Joschua H <54814804+joschiservice@users.noreply.github.com>
2022-11-07 12:00:23 +0100

    feat(horizon): use handoff tokens for auth

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Http;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
 
@@ -34,14 +35,33 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewHorizon', function ($user = null) {
-            if (!request()->session()->has('s2sc_access_code') || config('s2sc.token') == null) {
-                if (!request()->has('accessCode') || request()->accessCode != config('s2sc.token')) return false;
+            $s2sc_token = config('s2sc.token');
+            if (!request()->session()->has('horizon_authenticated') || $s2sc_token == null) {
+                if (!request()->has('token')) return false;
 
-                request()->session()->put('s2sc_access_code', request()->accessCode);
+                $validatedRequest = request()->validate([
+                    'token' => 'required|uuid'
+                ]);
+
+                // validate token
+                $response = Http::withHeaders([
+                    'S2SC-Token' => $s2sc_token
+                ])->asJson()->acceptJson()
+                    ->post('https://vcc-online.eu/api/s2sc/handoff-token/check', [
+                        'service' => 'vtcmanager',
+                        'tool' => 'horizon',
+                        'token' => $validatedRequest['token'],
+                        'user_ip' => request()->ip()
+                    ]);
+
+                if ($response->status() !== 204) return false;
+
+                request()->session()->put('horizon_authenticated', true);
+
                 redirect(config('horizon.path'));
             }
 
-            return config('s2sc.token') != null && request()->session()->get('s2sc_access_code') == config('s2sc.token');
+            return config('s2sc.token') != null && request()->session()->get('horizon_authenticated') == true;
         });
     }
 }
